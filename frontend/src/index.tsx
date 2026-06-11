3b9e7334a2c375ac1d0f9b3ef21ac8085c1212dd
nicolasbu2000 <nicolasbusch2000@icloud.com>
2026-06-11 12:10:33 +0200

    update react 18

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,4 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { DeleteConfirmProvider } from "./provider/deleteConfirmProvider";
 import { MainProvider } from "./provider/mainProvider";
@@ -8,22 +8,27 @@ import { TodoProvider } from "./provider/todoProvider";
 import "./styles/styles.css";
 import * as serviceWorkerRegistration from "./worker/serviceWorkerRegistration";
 
-render(
-  <MainProvider>
-    <ThemeProvider>
-      <DeleteConfirmProvider>
-        <TodoProvider>
-          <TodoListProvider>
-            <App />
-          </TodoListProvider>
-        </TodoProvider>
-      </DeleteConfirmProvider>
-    </ThemeProvider>
-  </MainProvider>,
-  document.getElementById("root")
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+    throw new Error("Root element with id 'root' not found");
+}
+
+createRoot(rootElement).render(
+    <MainProvider>
+        <ThemeProvider>
+            <DeleteConfirmProvider>
+                <TodoProvider>
+                    <TodoListProvider>
+                        <App />
+                    </TodoListProvider>
+                </TodoProvider>
+            </DeleteConfirmProvider>
+        </ThemeProvider>
+    </MainProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
+// Learn more about service workers
 serviceWorkerRegistration.register();
